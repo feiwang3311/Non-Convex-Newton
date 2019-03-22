32c48b3ced64fa1003c4bcdb151278fde85a9d70
feiwang3311 <wang603@purdue.edu>
2019-03-21 23:41:12 -0400

    remove tensorflow dependency

--- HFD/scripts.sh
+++ HFD/scripts.sh
@@ -1,11 +1,2 @@
-virtualenv --system-site-packages -p python3 ./venv
-
-source ./venv/bin/activate
-
-pip install --upgrade pip
-pip list
-pip install --upgrade tensorflow
-python -c "import tensorflow as tf; tf.enable_eager_execution(); print(tf.reduce_sum(tf.random_normal([1000, 1000])))"
-
 echo "Note: Maybe downloading cifar10_data"
 python3 generate_cifar10_data.py --data-dir cifar10_data
